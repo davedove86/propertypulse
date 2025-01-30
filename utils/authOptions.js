a7a3580f5cac06a6e4a6e901fc75c7c3ece9411f
Dave Dove <info@dovedesign.io>
2025-01-30 10:33:01 +0000

    added login/logout with profile image and protected routes

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from 'next-auth/providers/google';
+import connectDB from '@/config/database';
+import User from '@/models/User';
 
 export const authOptions = {
   providers: [
@@ -14,19 +16,33 @@ export const authOptions = {
       },
     }),
   ],
-  callback: {
+  callbacks: {
     // invoked on successful sign in
     async signIn({ profile }) {
       // connect to database
+      await connectDB();
       // check if user exisits
+      const userExists = await User.findOne({ email: profile.email });
       // if not, create user
+      if (!userExists) {
+        const username = profile.name.slice(0, 20);
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // Return true to allow sign in
+      return true;
     },
     // Session callback function that modifies the session object
-    async session({ session, token, user }) {
+    async session({ session }) {
       // get the user from the database
+      const user = await User.findOne({ email: session.user.email });
       // assign user ID from session
+      session.user.id = user._id.toString();
       // Return the session
+      return session;
     },
   },
 };
